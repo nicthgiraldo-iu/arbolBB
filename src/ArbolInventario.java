0081140ad8352da3a360b46ecd05414bcca66164
nicthgiraldo-iu <nicth.giraldo@est.iudigital.edu.co>
2026-09-23 19:53:31 -0500

    se realizan crea la funcionalidad de guardar un producto, se realizan pruebas unitarias con errores menores corregidos, se indican los comentarios necesarios para el entendimiento del código

--- src/ArbolInventario.java
+++ src/ArbolInventario.java
@@ -1,4 +1,47 @@
 
 public class ArbolInventario{
-    
+    private Producto raiz;
+
+    //se crea el constructor y se inicializa vácio, ya que al empezar nuestro arbol aun no tiene raiz
+    public ArbolInventario(){
+        this.raiz = null;
+    }
+
+    //validacion que nos sirve para no tener que recorrer nada si la raiz esta en nula, ya que esto indica 
+    //que efectivamente no se han ingresado datos
+    public boolean estaVacio() {
+        return raiz == null;
+    }
+
+    //se piden los datos principales de id y nombre para crear el nodo y empezar con el arbol
+    public void insertar(int id, String nombre){
+        //se guarda la raiz, con esto se asegura de como guardar el proximo dato
+        //menor a la izquierda, mayor a la derecha
+        //se utiliza la metodología enseñada de recursividad en los metodos creados
+        //se guarda el nodo registrado inmediato para determinar la raiz del siguiente nodo
+        raiz = insertarR(raiz, id, nombre);
+    }
+
+    //actual => recive el último nodo guardado para determinar donde guardar el siguiente
+    //menor a la izquierda, mayor valor a la derecha
+    private Producto insertarR(Producto actual, int id, String nombre) {
+        //ingresa en este if si es el primer registro, creando el primer nodo o raiz primera de nuestro arbol
+        if (actual == null) {
+            return new Producto(id, nombre);
+        }
+        
+        //si ya existe un nodo o raiz, se evalúa si va a la izquierda o derecha dependiendo si es mayor o menor
+        if (id < actual.getId()) {
+            //al detectar que es menor, se guarda el nodo en el izquierdo del nodo actual para crear la ramificacion del arbol
+            actual.setIzquierdo(insertarR(actual.getIzquierdo(), id, nombre));
+        }
+        else if (id > actual.getId()) {
+            actual.setDerecho(insertarR(actual.getDerecho(), id, nombre));
+        }
+
+        //se retorna el nodo con sus respectivas ramas
+        return actual;
+    }
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        ArbolInventario arbol = new ArbolInventario();
         int opcion = -1;
 
         while (opcion != 0) {
@@ -13,6 +14,36 @@ public class Main {
             System.out.println("3. Buscar Producto");
             System.out.println("0. Salir");
             System.out.print("Seleccione una opción: ");
+
+            try {
+                opcion = sc.nextInt();
+            } catch (Exception e) {
+                System.out.println("Entrada inválida. Debe ingresar un número.");
+                sc.next(); // Limpia el Scanner
+                // Vuelve a mostrar el menú
+                continue;
+            }
+
+            switch (opcion) {
+                case 1: // Registrar producto
+                    System.out.print("Ingrese el ID del producto: ");
+                    try {
+                        // se capturan los datos de id y nombre para crear el nodo
+                        int id = sc.nextInt();
+                        sc.nextLine();
+                        System.out.print("Nombre del producto: ");
+                        String nombre = sc.nextLine();
+                        arbol.insertar(id, nombre);
+                        System.out.println("Registrado con éxito.");
+                    } catch (Exception e) {
+                        System.out.println("ID debe ser un número válido.");
+                        sc.next();
+                    }
+                    break;
+                default: // Opción no válida
+                    System.out.println("Opción no válida.");
+            }
         }
+        sc.close();
     }
 }
